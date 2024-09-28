611148513cd1b7bb4169285631138bbb99d6f550
CERN <2440539709@qq.com>
2024-09-28 16:32:31 +0800

    two

--- java/src/main/java/liu/com/excell.java
+++ java/src/main/java/liu/com/excell.java
@@ -9,65 +9,50 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.joda.time.DateTime;
 import org.junit.Test;
 
-import java.io.IOException;
+import java.io.*;
+
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-
 public class excell {
-    static String PATH="java/src/main/resources/rating.xlsx";
+    static String PATH= "src/main/resources/rating.xlsx";
     public static int length=0;
     public static int weight=0;
     //2023届数据
     public static man[][] Man=new man[2][3];
-    public static class man{
-        public String name;
-        public String codeforces;
-        public String nowcode;
-        public String atcode;
-        public void Xmm() throws Exception {
-            //获取文件流
-            FileInputStream inputStream=new FileInputStream(PATH);
-            //1，创建一个工作簿
-            Workbook  workbook =new XSSFWorkbook(inputStream);
-            //2,得到表
-            for(int i=0;i<2;i++)
-            {
-                Sheet sheet=workbook.getSheetAt(i);
-                length=sheet.getLastRowNum();
-                for(int j=0;j<sheet.getLastRowNum();j++)
-                {
-                    Row row=sheet.getRow(j+1);
-                    Man[i][j]=new man();
-                    weight=row.getLastCellNum();
-                    for(int k=0;k<row.getLastCellNum();k++) {
-                        Cell cell = row.getCell(k);
-                        String str = cell.getStringCellValue();
-                        //System.out.println(k+" "+str);
-                        if(k==0)
-                        {
-                            Man[i][j].name=str;
-                        }
-                        else if (k==1)
-                        {
-                            Man[i][j].codeforces=str;
-                        }
-                        else if(k==2)
-                        {
-                            Man[i][j].nowcode=str;
-                        }
-                        else if(k==3)
-                        {
-                            Man[i][j].atcode=str;
-                        }
-                        //System.out.println(Man[i][j].name+","+Man[i][j].codeforces+","+Man[i][j].nowcode+","+Man[i][j].atcode);
+
+
+    public void EXC() throws Exception {
+        //获取文件流
+        FileInputStream inputStream = new FileInputStream(PATH);
+        //1，创建一个工作簿
+        Workbook workbook = new XSSFWorkbook(inputStream);
+        //2,得到表
+        for (int i = 0; i < 2; i++) {
+            Sheet sheet = workbook.getSheetAt(i);
+            length = sheet.getLastRowNum();
+            for (int j = 0; j < sheet.getLastRowNum(); j++) {
+                Row row = sheet.getRow(j + 1);
+                Man[i][j] = new man();
+                weight = row.getLastCellNum();
+                for (int k = 0; k < row.getLastCellNum(); k++) {
+                    Cell cell = row.getCell(k);
+                    String str = cell.getStringCellValue();
+                    //System.out.println(k+" "+str);
+                    if (k == 0) {
+                        Man[i][j].name = str;
+                    } else if (k == 1) {
+                        Man[i][j].codeforces = str;
+                    } else if (k == 2) {
+                        Man[i][j].nowcode = str;
+                    } else if (k == 3) {
+                        Man[i][j].atcode = str;
                     }
+                    //System.out.println(Man[i][j].name+","+Man[i][j].codeforces+","+Man[i][j].nowcode+","+Man[i][j].atcode);
                 }
             }
+        }
 //        for(int i=0;i<2;i++)
 //        {
 //            for(int j=0;j<3;j++)
@@ -75,7 +60,13 @@ public class excell {
 //                System.out.println(Man[i][j].name+","+Man[i][j].codeforces+","+Man[i][j].nowcode+","+Man[i][j].atcode);
 //            }
 //        }
-            inputStream.close();
-        }
+        inputStream.close();
+    }
+
+    public static class man {
+        public String name;
+        public String codeforces;
+        public String nowcode;
+        public String atcode;
     }
 }
---
+++ java/src/test/java/RUN.java
@@ -0,0 +1,137 @@
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.google.gson.Gson;
+import liu.com.JOSN;
+import liu.com.excell;
+
+public class RUN {
+
+    static Scanner sc = new Scanner(System.in);
+    static String codeforces_URL="https://codeforces.com/api/user.info?checkHistoricHandles=false&handles=";
+    static String nowcode_URL="https://ac.nowcoder.com/acm/user/=";
+    static String atcode_URL="https://atcoder.jp/users/=";
+    static Fuck [][]fuck=new Fuck[2][3];
+    static codeforces_color [] cf_color=new codeforces_color[10];
+    static nowcode_color [] nc_color=new nowcode_color[5];
+    public static void main(String[] args) throws Exception {
+        for(int i=0;i<10;i++)
+        {
+            cf_color[i]=new codeforces_color();
+        }
+        //
+        cf_color[0].rank="unrated";
+        cf_color[1].rank="newbie";
+        cf_color[2].rank="pupil";
+        cf_color[3].rank="specialist";
+        cf_color[4].rank="expert";
+        cf_color[5].rank="candidate master";
+        cf_color[6].rank="master";
+        cf_color[7].rank="international master";
+        cf_color[8].rank="grandmaster";
+        cf_color[9].rank="headquarters";
+        //
+        for(int i=0;i<5;i++)
+        {
+            nc_color[i]=new nowcode_color();
+        }
+        //
+        nc_color[0].rank="rate-score1";
+        nc_color[1].rank="rate-score2";
+        nc_color[2].rank="rate-score3";
+        nc_color[3].rank="rate-score4";
+        nc_color[4].rank="rate-score5";
+        //fuck
+        excell x = new excell();
+        JOSN api = new JOSN();
+        x.EXC();
+        for(int i=0;i<2;i++)
+        {
+            for(int j=0;j<x.length;j++)
+            {
+                fuck[i][j]=new Fuck();
+                fuck[i][j].Name=x.Man[i][j].name;
+                fuck[i][j].CodeForces=x.Man[i][j].codeforces;
+                for(int k=1;k<x.weight;k++)
+                {
+                    if (k==1)//codeforces
+                    {
+                        String codeforces_json=api.run(codeforces_URL+x.Man[i][j].codeforces);
+                        JSONObject jsonObject = JSONObject.parseObject(codeforces_json);
+                        if(jsonObject.get("status").equals("OK"))
+                        {
+                            JSONArray jsonArray = jsonObject.getJSONArray("result");
+                            JSONObject jsonObject1 = new JSONObject();
+                            for(int l=0;l<jsonArray.size();l++)
+                            {
+                                jsonObject1 = jsonArray.getJSONObject(l);
+                            }
+                            String o1=jsonObject1.getString("rating");
+                            fuck[i][j].CodeForces_rating=o1;
+                            String o2=jsonObject1.getString("rank");
+                            fuck[i][j].CodeForces_rank=o2;
+                            for(int e=0;e<10;e++)
+                            {
+                                if(cf_color[e].rank.equals(o2))
+                                {
+                                    fuck[i][j].CodeForces_color=cf_color[e].color;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+//                    else if(k==2)//nowcode
+//                    {
+//                        String nowcode_json=api.run(nowcode_URL+x.Man[i][j].nowcode);
+//                        JSONObject jsonObject = JSONObject.parseObject(nowcode_json);
+//
+//                    }
+//                    else if(k==3)//atcode
+//                    {
+//                        String atcode_json=api.run(atcode_URL+x.Man[i][j].atcode);
+//                    }
+                }
+            }
+        }
+//        for(int i=0;i<2;i++)
+//        {
+//            for(int j=0;j<x.length;j++)
+//            {
+//                System.out.println(fuck[i][j].Name+" "+fuck[i][j].CodeForces+" "+fuck[i][j].CodeForces_rating+" "+fuck[i][j].CodeForces_rank);
+//            }
+//        }
+        Gson gson = new Gson();
+        String json = gson.toJson(fuck);
+
+        System.out.println(json);
+    }
+
+    static class Fuck
+    {
+        String Name;
+        String CodeForces;
+        String CodeForces_rating;
+        String CodeForces_color;
+        String CodeForces_rank;
+        String Nowcode;
+        String Nowcode_rating;
+        String Nowcode_color;
+        String Nowcode_rank;
+        String Atcode;
+        String Atcode_rating;
+    }
+    static class codeforces_color
+    {
+        String rank;
+        String color;
+    }
+
+    static class nowcode_color
+    {
+        String rank;
+        String color;
+    }
+}
